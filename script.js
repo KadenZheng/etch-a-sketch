b5b52ef42ec74520b4ebe7402fcbadb49a216cfe
Kaden Zheng <KadenZheng@mail.fresnostate.edu>
2024-06-10 14:05:06 +0200

    added rainbow squares w/ click

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Queries
-    let body = document.querySelector("body");
     let container = document.querySelector(".container");
-
-    // Elements created
-    let square = document.createElement("div");
 
     // Modifications
     for (let i = 0; i < 16; i++) {
         addSquare(container);
     }
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        let state = 0;
+
+        square.addEventListener("mouseenter", () => {
+            state = 0;
+            var randomColor = "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+            square.style.cssText = `background-color: ${randomColor}`;
+        });
+
+        square.addEventListener("mouseleave", () => {
+            if (state === 0) {
+                square.style.cssText = "background-color: black; transition: background-color 0.5s ease;";
+            } else {
+                return;
+            }
+        });
+
+        square.addEventListener("click", () => {
+            state = 1;
+            square.style.cssText = "";
+            square.style.cssText = "background-color: white;";
+        });
+    });
 });
 
 const addSquare = (container) => {
